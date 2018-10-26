cc64475600e89b482c874add96bb80b207e79b70
sveta0112 <sveta0112@hotmail.com>
2018-10-26 15:09:02 -0700

    Clean up code, got rid off some folders, made changes in seed.js file

--- database/seed.js
+++ database/seed.js
@@ -2,15 +2,15 @@ const faker = require('faker');
 const fs = require('fs');
 const Restaurent_Gallery = require('./Image.js')
 
-var ourfaker =[];
-  var imageCounter = 1;
+let ourfaker =[];
+  let imageCounter = 1;
   for (i = 1; i < 101; i++) {
-    var data = {};
+    let data = {};
     data.res_id = i;
-      var images_array = [];
+      let images_array = [];
       for (j = 1; j < faker.random.number(10) + 6; j++) {
-            var image_object ={};
-            image_object.image_id = (i* 100) + j;
+            let image_object ={};
+            image_object.image_id = (i * 100) + j;
             image_object.image_url = 'https://s3-us-west-1.amazonaws.com/lanakhan/img' + imageCounter + '.jpg';
             if (imageCounter == 57) {
               imageCounter = 0;
@@ -22,8 +22,9 @@ var ourfaker =[];
           }
     data.images_array = images_array;
     ourfaker.push(data);
+    console.log(ourfaker);
     
   }
-Restaurent_Gallery.create(ourfaker)
+Restaurent_Gallery.create(ourfaker);
 
 
--- server/serverIndex.js
+++ server/serverIndex.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-var faker = require('faker');
+const faker = require('faker');
 
 const Restaurent_Gallerys = require('../database/Image.js');
 
@@ -18,9 +18,9 @@ app.use(express.static(__dirname + '../public'));
 app.get('/api/rest', function(req, res) {
   Restaurent_Gallerys.find({})
         .exec()
-      .then(restaurents => {
+      .then(restaurants => {
           res.status(200).json({
-              restaurents
+              restaurants
           });
       })
       .catch(err => {
@@ -29,16 +29,16 @@ app.get('/api/rest', function(req, res) {
 });
 
 app.get('/api/user', function(req, res) {
-  var ourfaker =[];
-  var imageCounter = 1;
+  let ourfaker =[];
+  let imageCounter = 1;
   for (i = 1; i < 101; i++) {
-    var data = {};
+    let data = {};
     data.res_id = i;
-      var images_array = [];
+      let images_array = [];
       for (j = 1; j < 11; j++) {
-            var image_object ={};
+            let image_object ={};
             image_object.image_id = (i* 100) + j;
-            //image_object.image_url = faker.image.food();
+            
             image_object.image_url = 'https://s3-us-west-1.amazonaws.com/lanakhan/img' + imageCounter + '.jpg';
             if (imageCounter == 57) {imageCounter = 0;}
             imageCounter++;
