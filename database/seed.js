b09f723b08288e7e00ae30b35cf1935bf4e6bee3
sveta0112 <sveta0112@hotmail.com>
2018-10-26 15:25:47 -0700

    fixed indentation

--- database/seed.js
+++ database/seed.js
@@ -9,21 +9,20 @@ let ourfaker =[];
     data.res_id = i;
       let images_array = [];
       for (j = 1; j < faker.random.number(10) + 6; j++) {
-            let image_object ={};
-            image_object.image_id = (i * 100) + j;
-            image_object.image_url = 'https://s3-us-west-1.amazonaws.com/lanakhan/img' + imageCounter + '.jpg';
-            if (imageCounter == 57) {
-              imageCounter = 0;
-            }
-            imageCounter++;
-            image_object.description = faker.lorem.sentence();
-            image_object.taken_by = faker.name.findName();
-            images_array.push(image_object);
-          }
+        let image_object ={};
+        image_object.image_id = (i * 100) + j;
+        image_object.image_url = 'https://s3-us-west-1.amazonaws.com/lanakhan/img' + imageCounter + '.jpg';
+        if (imageCounter == 57) {
+          imageCounter = 0;
+        }
+        imageCounter++;
+        image_object.description = faker.lorem.sentence();
+        image_object.taken_by = faker.name.findName();
+        images_array.push(image_object);
+      }
     data.images_array = images_array;
     ourfaker.push(data);
     console.log(ourfaker);
-    
   }
 Restaurent_Gallery.create(ourfaker);
 
--- server/serverIndex.js
+++ server/serverIndex.js
@@ -17,15 +17,15 @@ app.use(express.static(__dirname + '../public'));
 // after database is populated it will populate..
 app.get('/api/rest', function(req, res) {
   Restaurent_Gallerys.find({})
-        .exec()
-      .then(restaurants => {
-          res.status(200).json({
-              restaurants
-          });
-      })
-      .catch(err => {
-          console.log(err)
+    .exec()
+    .then(restaurants => {
+      res.status(200).json({
+          restaurants
       });
+    })
+    .catch(err => {
+        console.log(err)
+    });
 });
 
 app.get('/api/user', function(req, res) {
@@ -36,16 +36,16 @@ app.get('/api/user', function(req, res) {
     data.res_id = i;
       let images_array = [];
       for (j = 1; j < 11; j++) {
-            let image_object ={};
-            image_object.image_id = (i* 100) + j;
-            
-            image_object.image_url = 'https://s3-us-west-1.amazonaws.com/lanakhan/img' + imageCounter + '.jpg';
-            if (imageCounter == 57) {imageCounter = 0;}
-            imageCounter++;
-            image_object.description = faker.lorem.sentence();
-            image_object.taken_by = faker.name.findName();
-            images_array.push(image_object);
-          }
+        let image_object ={};
+        image_object.image_id = (i* 100) + j;
+        
+        image_object.image_url = 'https://s3-us-west-1.amazonaws.com/lanakhan/img' + imageCounter + '.jpg';
+        if (imageCounter == 57) {imageCounter = 0;}
+        imageCounter++;
+        image_object.description = faker.lorem.sentence();
+        image_object.taken_by = faker.name.findName();
+        images_array.push(image_object);
+      }
     data.images_array = images_array;
     ourfaker.push(data);
   }
