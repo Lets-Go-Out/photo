cd39900656c5ef024a20a834113ba030d3649f95
sveta0112 <sveta0112@hotmail.com>
2018-11-10 14:34:15 -0800

    k

--- client/components/fetch.js
+++ client/components/fetch.js
@@ -2,7 +2,7 @@ const $ = require("jquery");
 //////
 export const resImages = (rest_id, cb) => {
   $.ajax({
-    url: `http://ec2-54-164-63-168.compute-1.amazonaws.com/restaurants/${rest_id}/gallery`,
+    url: `http://ec2-54-164-63-168.compute-1.amazonaws.com/restaurants/1/gallery`,
     method: "GET",
     success: resImages => {
       cb(rest_id, resImages);
