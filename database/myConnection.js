78bbc7491a0dcf350b86a2861dc3a4569f971edb
sveta0112 <sveta0112@hotmail.com>
2019-03-22 19:38:51 -0700

    new

--- database/myConnection.js
+++ database/myConnection.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose");
-const dockerdb = "photodb:27017";
+//const dockerdb = "photodb:27017";
 //const dockerdb = "localhost"
 
-const mongoUri = `mongodb://${dockerdb}/restaurant_images`;
+//const mongoUri = `mongodb://${dockerdb}/restaurant_images`;
+const mongoUri = `mongodb://localhost/restaurant_images`;
 
 const db = mongoose.connect(mongoUri);
 
