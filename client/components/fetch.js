62e6088892be4fa1b46e36df3d841adb8281bdab
sveta0112 <sveta0112@hotmail.com>
2019-03-23 00:21:10 -0700

    17

--- client/components/fetch.js
+++ client/components/fetch.js
@@ -2,7 +2,7 @@ const $ = require("jquery");
 //`http://localhost:3004/restaurants/${rest_id}/gallery`
 export const resImages = (rest_id, cb) => {
   $.ajax({
-    url: `http://localhost:3004/15`,
+    url: `https://photo-demo.herokuapp.com/17`,
     method: "GET",
     success: resImages => {
       cb(rest_id, resImages);
